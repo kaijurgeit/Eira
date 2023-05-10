67a60ff3d7be85350d9541c9a60b147836133892
Agent49 <kai.jurgeit@t-online.de>
2023-05-10 13:27:03 +0200

    C++: FEiraInputAction maps InputTag to InputAction,  UInputConfig stores array of FEiraInputActions, UEiraInputComponent provides a function to Bind input to a Tag, EiraInputComponent selected in Project Settings.

---
+++ Source/Eira/Game/EiraInputComponent.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EiraInputConfig.h"
+#include "EnhancedInputComponent.h"
+#include "UObject/Object.h"
+#include "EiraInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIRA_API UEiraInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	template<class UserClass, typename FuncType>
+	void BindNativeAction(const UEiraInputConfig* InputConfig, const FGameplayTag& InputTag, ETriggerEvent TriggerEvent, UserClass* Object, FuncType Func);	
+};
+
+template <class UserClass, typename FuncType>
+void UEiraInputComponent::BindNativeAction(const UEiraInputConfig* InputConfig, const FGameplayTag& InputTag, ETriggerEvent TriggerEvent, UserClass* Object, FuncType Func)
+{
+	check(InputConfig);
+	if(const UInputAction* IA = InputConfig->FindInputActionForTag(InputTag))
+	{
+		BindAction(IA, TriggerEvent, Object, Func);
+	}
+}
---
+++ Source/Eira/Game/EiraInputConfig.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "UObject/Object.h"
+#include "EiraInputConfig.generated.h"
+
+class UInputAction;
+/**
+ * FEiraInputAction
+ *
+ *	Struct used to map an input action to a gameplay input tag.
+ */
+USTRUCT(BlueprintType)
+struct FEiraInputAction
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditDefaultsOnly)
+	const UInputAction* InputAction = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, Meta = (Category = "InputTag"))
+	FGameplayTag InputTag;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class EIRA_API UEiraInputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	// Returns the first Input Action associated with a given tag.
+	const UInputAction* FindInputActionForTag(const FGameplayTag& InputTag) const;
+
+public:
+	// List of input actions used by the owner. These input actions are mapped to a gameplay tag and must be manually bound.
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Meta = (TitleProperty = "InputAction"))
+	TArray<FEiraInputAction> NativeInputActions;
+};
