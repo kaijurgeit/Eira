66078b0b7ab8704a4f3aff61c2e65beb6087bbf6
Agent49 <kai.jurgeit@t-online.de>
2023-06-14 09:57:29 +0200

    BP and C++: Added GA_Pickup, before changing input config.

---
+++ Source/Eira/System/EiraUtilities.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EiraUtilities.h"
+
+void UEiraUtilities::OrderByDistance(AActor* OriginActor, TArray<AActor*>& OutArray)
+{
+	OutArray.Sort([OriginActor](const AActor& A, const AActor& B)
+	{
+		const float DistanceA = A.GetDistanceTo(OriginActor);
+		const float DistanceB = B.GetDistanceTo(OriginActor);
+		return DistanceA > DistanceB; 
+	});
+}
---
+++ Source/Eira/System/EiraUtilities.h
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "EiraUtilities.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EIRA_API UEiraUtilities : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = Helpers)
+	static void OrderByDistance(AActor* OriginActor, TArray<AActor*>& OutArray);
+};
